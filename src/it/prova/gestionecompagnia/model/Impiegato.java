6d14044b2a14c0e3035604e6dd9a626089c22163
AlessioConti <alessioconti02@yahoo.it>
2022-05-17 14:25:51 +0200

    Create classi modello di Compagnia e Impiegato

---
+++ src/it/prova/gestionecompagnia/model/Impiegato.java
@@ -0,0 +1,82 @@
+package it.prova.gestionecompagnia.model;
+
+import java.util.Date;
+
+public class Impiegato {
+
+	private Long id;
+	private String nome;
+	private String cognome;
+	private Date dataNascita;
+	private Date dataAssunzione;
+	private Compagnia compagnia;
+
+	public Compagnia getCompagnia() {
+		return compagnia;
+	}
+
+	public void setCompagnia(Compagnia compagnia) {
+		this.compagnia = compagnia;
+	}
+
+	public Impiegato() {
+	}
+
+	public Impiegato(String nome, String cognome, Date dataNascita, Date dataAssunzione) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.dataAssunzione = dataAssunzione;
+	}
+
+	public Impiegato(Long id, String nome, String cognome, Date dataNascita, Date dataAssunzione) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.dataAssunzione = dataAssunzione;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(Date dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public Date getDataAssunzione() {
+		return dataAssunzione;
+	}
+
+	public void setDataAssunzione(Date dataAssunzione) {
+		this.dataAssunzione = dataAssunzione;
+	}
+
+}
